c2e8f7d2b9b7ce543e77b00f0aaea6b221eacde7
LuceroLuciano <lucero.luciano@umb.edu.mx>
2022-04-28 17:07:28 -0500

    Paso 10: Renderizando la prueba de unidad

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -1,31 +1,9 @@
-/*
-Requerimiento 1: 
-    Se necesita un modelo `User` para guardar la información de un usuario 
-    de nuestra app: `id`, `username`, `name`, `bio`, `dateCreated` y `lastUpdated`. 
-
-Requerimiento 2: 
-    Las propiedades `dateCreated` y `lastUpdated` deberán ser datos de tipo fecha que 
-    guarden el momento en que se instancie un nuevo objeto de la clase `User`
-
- Requerimiento 3: 
-    Cada objeto tipo user necesita los siguietes gettes: `getUsername`, `getBio`, 
-    `getDateCreated`, `getLastUpdated`. Solución: Agrega los getters 
-    regresando la propiedad indicada, no olvides usar `this`.
-
-
-Requerimiento 4: 
-    Cada objeto necesita los siguientes setters: `setUsername` y `setBio`, para actualizar dichas propiedades.
-    Solución: Agrega los setters usando la palabra `set` y modificando el atributo indicado.
-*/
-
 const User = require('./../../app/models/User')
 
 describe("Unit Test for User class", () => {
     test('Req 1 & 2: Create an User object', () => {
-        //Aqui invocamos el código que se va a usar en la app
-        const user = new User(1, "Luz", "Lucero","bio")
-        
-        // Aqui validas los resultao de este código
+        const user = new User(1, "Luz", "Lucero","bio")        
+    
         expect(user.id).toBe(1)
         expect(user.username).toBe("Luz")
         expect(user.name).toBe("Lucero")
